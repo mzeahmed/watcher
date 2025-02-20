b29a90502ac57accf4b8566cad3865596b6edb90
MzeAhmed <amze@corp.free.fr>
2025-02-20 13:05:45 +0100

    :necktie: Update allowed file extensions

--- watcher.php
+++ watcher.php
@@ -5,17 +5,10 @@
  * @package           Watcher
  * @wordpress-plugin
  * Plugin Name:       Watcher
- * Description:       This plugin is designed to help developers monitor and synchronize files between development and the WP plugins and themes directories.
- * Version:           1.0.5
- * Author:            Ahmed Mze
- * Author URI:
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       watcher
- * Domain Path:       /resources/i18n
- * Requires PHP:      8.3.0
- * Requires WP:       6.0.0
- * Namespace:         Watcher
+ * Description:       This plugin is designed to help developers monitor and synchronize files between development and the WP plugins and
+ *     themes directories. Version:           1.0.6 Author:            Ahmed Mze Author URI: License:           GPL-2.0+ License URI:
+ *     http://www.gnu.org/licenses/gpl-2.0.txt Text Domain:       watcher Domain Path:       /resources/i18n Requires PHP:      8.3.0
+ *     Requires WP:       6.0.0 Namespace:         Watcher
  */
 
 declare(strict_types=1);
