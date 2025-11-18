149d7ec5acbf46d21af090343075e2add54c310f
MzeAhmed <houdjiva@gmail.com>
2025-11-18 23:00:36 +0100

    Add deletion logique

--- src/Watcher.php
+++ src/Watcher.php
@@ -78,13 +78,61 @@ class Watcher
             $destinationFile = $destinationPath . $relativePath;
 
             // Create the destination directory if it does not exist
-            if (!file_exists(\dirname($destinationFile))) {
-                mkdir(dirname($destinationFile), 0777, true);
+            if (
+                !file_exists(\dirname($destinationFile))
+                && !mkdir($concurrentDirectory = dirname($destinationFile), 0777, true)
+                && !is_dir($concurrentDirectory)
+            ) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
             }
 
             // Copy the file if it does not exist or if it is newer than the destination file
             if (!file_exists($destinationFile) || filemtime($file) > filemtime($destinationFile)) {
                 copy($file, $destinationFile);
+            }
+        }
+    }
+
+    private function syncDeletions(array $sourceFiles, string $destinationPath, string $sourcePath, array $exclude): void
+    {
+        $sourceSet = [];
+        foreach ($sourceFiles as $file) {
+            $sourceSet[str_replace($sourcePath, '', $file)] = true;
+        }
+
+        $destFiles = Utils::listFilesWithRecursiveIteratorIterator(
+            $destinationPath,
+            self::ALLOWED_EXTENSIONS,
+            $exclude
+        );
+
+        foreach ($destFiles as $destFile) {
+            $relative = str_replace($destinationPath, '', $destFile);
+            if (!isset($sourceSet[$relative])) {
+                @unlink($destFile);
+            }
+        }
+
+        $this->removeEmptyDirectories($destinationPath);
+    }
+
+    private function removeEmptyDirectories(string $path): void
+    {
+        if (!is_dir($path)) {
+            return;
+        }
+
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($path, \FilesystemIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($iterator as $item) {
+            if ($item->isDir()) {
+                $dir = $item->getPathname();
+                if (!(new \FilesystemIterator($dir))->valid()) {
+                    @rmdir($dir);
+                }
             }
         }
     }
@@ -105,5 +153,6 @@ class Watcher
             $exclude
         );
         $this->syncFiles($files, $destination, $source);
+        $this->syncDeletions($files, $destination, $source, $exclude);
     }
 }
