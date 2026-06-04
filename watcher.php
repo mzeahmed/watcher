51c55434e769561a10ea97698fb5139798c94a72
Ahmed Mze <houdjiva@gmail.com>
2026-06-04 17:24:21 +0200

    Update version

--- watcher.php
+++ watcher.php
@@ -8,7 +8,7 @@
  * @wordpress-plugin
  * Plugin Name:       Watcher
  * Description:       This plugin is designed to help developers monitor and synchronize files between development and the WP plugins and themes directories.
- * Version:           1.0.7
+ * Version:           1.0.8
  * Author:            Ahmed Mze
  * Author URI:
  * License:           GPL-2.0+
